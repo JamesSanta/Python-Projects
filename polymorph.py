0293940ba60d5a2a87b892da5e9d70280f26466c
JamesSanta <jamessanta@hotmail.com>
2023-07-25 23:26:27 -0400

    Update polymorph.py
    Updated assignment to include use of parent method in child classes

--- polymorph.py
+++ polymorph.py
@@ -7,19 +7,31 @@ class Mammal:
     species = "Unknown"
     num_legs = 0
     has_hair = False
-    diet = "Unknown"        
-
+    diet = "Unknown"
+    
+    def mammalSpecifics(self):
+        return ("This is a {}, it has {} legs, it is: {} that it has hair, and its diet is {}.".format(self.species,self.num_legs,self.has_hair,self.diet))
+              
 #Child class
 class WingedAnimal(Mammal):
-
+    
     wing_span = 0
     can_swim = False
+    
+    def mammalSpecifics(self):
+        return ("This is a {}, it has {} legs, it is: {} that it has hair, its diet is {}, it has a wing span of {} inches, and it is: {} that it can swim.".format(self.species,self.num_legs,self.has_hair,self.diet,self.wing_span,self.can_swim))
 
 #Child class
 class Bat(WingedAnimal):
-    
+
     is_nocturnal = False
     echolocation = False
+    
+    def mammalSpecifics(self):
+        return ("This is a {}, it has {} legs, it is: {} that it has hair, its diet is {}, it has a wing span of {} inches, it is : {} that it can swim, it is: {} that it is nocturnal, and it is: {} that it uses echolocation.".format(self.species,self.num_legs,self.has_hair,self.diet,self.wing_span,self.can_swim,self.is_nocturnal,self.echolocation))
+
+        
+       
 
 # create an object of Mammal class
 human = Mammal()
@@ -49,8 +61,8 @@ brown_bat.is_nocturnal = True
 brown_bat.echolocation = True
 
 #Accessing the attributes
-print(human.diet)
-print(eagle.can_swim)
-print(brown_bat.echolocation)
+print(human.mammalSpecifics())
+print(eagle.mammalSpecifics())
+print(brown_bat.mammalSpecifics())
 
     
